90cb3258042cff1bcd0102bf93126ed0179bfd47
Duncan Jones <duncan@wortharead.com>
2019-04-03 18:10:30 -0400

    Tweak demo to allow better screenshots

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,15 +11,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Button Demo',
       home: Scaffold(
+        appBar: AppBar(
+          title: Text("flutter_auth_buttons"),
+        ),
+        backgroundColor: Color.fromARGB(0xFF, 0xF0, 0xF0, 0xF0),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              GoogleSignInButton(onPressed: () {}),
-              GoogleSignInButton(onPressed: () {}, darkMode: true),
-              FacebookSignInButton(onPressed: () {}),
-              TwitterSignInButton(onPressed: () {}),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(60.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                GoogleSignInButton(onPressed: () {}),
+                GoogleSignInButton(onPressed: () {}, darkMode: true),
+                FacebookSignInButton(onPressed: () {}),
+                TwitterSignInButton(onPressed: () {}),
+              ],
+            ),
           ),
         ),
       ),
