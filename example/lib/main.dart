f9d9b83bc93ec9075a3ef64240fa2a93e6773865
Duncan Jones <duncan@wortharead.com>
2019-04-04 19:25:54 -0400

    Update example to show stretched buttons

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,17 +16,53 @@ class MyApp extends StatelessWidget {
         ),
         backgroundColor: Color.fromARGB(0xFF, 0xF0, 0xF0, 0xF0),
         body: Center(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 60.0),
-            child: Column(
-              children: <Widget>[
-                GoogleSignInButton(onPressed: () {}),
-                GoogleSignInButton(onPressed: () {}, darkMode: true),
-                FacebookSignInButton(onPressed: () {}),
-                TwitterSignInButton(onPressed: () {}),
-              ],
-            ),
+          child: Column(
+            children: <Widget>[
+              Heading("Natural Size"),
+              Column(
+                children: <Widget>[
+                  GoogleSignInButton(onPressed: () {}),
+                  GoogleSignInButton(onPressed: () {}, darkMode: true),
+                  FacebookSignInButton(onPressed: () {}),
+                  TwitterSignInButton(onPressed: () {}),
+                ],
+              ),
+              SizedBox(height: 40.0),
+              Heading("Stretched by Parent"),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    GoogleSignInButton(onPressed: () {}),
+                    GoogleSignInButton(onPressed: () {}, darkMode: true),
+                    FacebookSignInButton(onPressed: () {}),
+                    TwitterSignInButton(onPressed: () {}),
+                  ],
+                ),
+              ),
+            ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class Heading extends StatelessWidget {
+  final String text;
+
+  Heading(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 22.0,
+          fontWeight: FontWeight.bold,
         ),
       ),
     );
